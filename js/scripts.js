0d559480ba0654d2fc6c562ab1eb7907b6ec492e
ignmand <ignaciofmandagaran@gmail.com>
2019-10-20 20:49:35 -0300

    casi por terminar

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+/* Mobile nav functions */
+function openNav(){
+    document.getElementById("mobile_menu").style.width = "100%";
+}
+
+function closeNav(){
+    document.getElementById("mobile_menu").style.width = "0";
+}
+
+/* Modal functions */
+    document.getElementById("button").addEventListener('click', function() {
+        document.querySelector('.modal').style.display = 'flex';
+        })
+    document.querySelector('.modal__close').addEventListener('click', function(){
+        document.querySelector('.modal').style.display = 'none';
+    })
+
+/* Social Media sharedcounts */
+
+   jQuery.sharedCount = function(url, fn) {
+    url = encodeURIComponent(url || location.href);
+    var domain = "//api.sharedcount.com/v1.0/";
+    var apikey = "f7a093c169e08c3e61db32c765638560b61d44ff"
+    var arg = {
+      data: {
+        url: url,
+        apikey: apikey
+      },
+      url: domain,
+      cache: true,
+      dataType: "json"
+    };
+    if ('withCredentials' in new XMLHttpRequest) {
+      arg.success = fn;
+    } else {
+      var cb = "sc_" + url.replace(/\W/g, '');
+      window[cb] = fn;
+      arg.jsonpCallback = cb;
+      arg.dataType += "p";
+    }
+    return jQuery.ajax(arg);
+  };
+
+   jQuery(document).ready(function($){
+    $.sharedCount(location.href, function(data){
+        console.log(data.twitter);
+        console.log(data.facebook);
+    });
+});
